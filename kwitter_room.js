447cbc7ce1192e84bd8e92a66053486cdb67e324
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2020-12-13 22:23:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyBlSZYbvYvV2xyw9EE8v1IXLliHyGK-lEI",
+    authDomain: "lets-chat-f0b13.firebaseapp.com",
+    projectId: "lets-chat-f0b13",
+    storageBucket: "lets-chat-f0b13.appspot.com",
+    messagingSenderId: "116860496838",
+    appId: "1:116860496838:web:fa18f804d4e146356aa0c7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
